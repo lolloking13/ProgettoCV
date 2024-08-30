c034a8e5d6ad4ef39170e251a430f4c52f28aab8
Lorenzo <77585164+lolloking13@users.noreply.github.com>
2024-08-30 18:44:31 +0200

    Started Experience Manager Development

---
+++ Assets/Scripts/ExperienceManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExperienceManager : MonoBehaviour
+{
+    public float timeLeft = 3f;
+
+    [Header("UI")]
+    public GameObject[] playerInputUI;
+    public Text iaInputText;
+    public Image countDownImageFill;
+
+    private void Start()
+    {
+        StartCoroutine(CountDown());
+    }
+
+    IEnumerator CountDown() {
+        while (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            yield return null;
+
+            countDownImageFill.fillAmount = timeLeft / 3;
+        }
+
+        IaInputSelection();
+    }
+
+    public void IaInputSelection()
+    {
+        int selection = Random.Range(0, 5);
+
+        string s = "";
+        if (selection == 0) s = "W";
+        else if (selection == 1) s = "A";
+        else if (selection == 2) s = "S";
+        else if (selection == 3) s = "D";
+
+        iaInputText.text = s;
+    }
+}
